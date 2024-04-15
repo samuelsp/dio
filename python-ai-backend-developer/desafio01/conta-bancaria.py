d4898b1e4731fbad23fba6051f5a423b75d06cf8
samuelsp <samp_81@yahoo.com.br>
2024-04-15 20:38:29 -0300

    desafio01 - conta bancaria refatorada

--- python-ai-backend-developer/desafio01/conta-bancaria.py
+++ python-ai-backend-developer/desafio01/conta-bancaria.py
@@ -12,7 +12,12 @@ print("###### SISTEMA BANCÁRIO DIO ######")
 
 while True:
     print(menu)
-    opcao = int(input("Informe uma opção: "))
+
+    try:
+        opcao = int(input("Informe uma opção: "))
+    except ValueError:
+        print("Digite um caracter válido!")
+        continue
 
     if opcao == 1:
         deposito = float(input("Informe o valor do depósito: "))
@@ -24,19 +29,25 @@ while True:
 
     elif opcao == 2:
         saque = float(input("Informe o valor do saque: "))
-        if saque > 0:
-            if saldo >= saque:
-                if limite >= saque:
-                    if numero_saques > 0:
-                        saldo -= saque
-                        numero_saques -= 1
-                        extrato += f"Saque: R$ {saque:.2f}\n"
-                    else:
-                        print("Operação falhou! Você excedeu a quantidade de saques diários.")
-                else:
-                    print("Operação falhou! Você excedeu o valor limite por transação de saque diário.")
-            else:
-                print("Operação falhou! Você não possui saldo suficiente.")
+
+        saldo_permitido   = saldo >= saque
+        limite_permitido  = limite >= saque
+        saques_permitidos = numero_saques > 0
+
+        if not saldo_permitido:
+            print("Operação falhou! Você não possui saldo suficiente.")
+
+        elif not limite_permitido:
+            print("Operação falhou! Você excedeu o valor limite por transação de saque diário.")
+
+        elif not saques_permitidos:
+            print("Operação falhou! Você excedeu a quantidade de saques diários.")
+
+        elif saque > 0:
+            saldo -= saque
+            numero_saques -= 1
+            extrato += f"Saque: R$ {saque:.2f}\n"
+
         else:
             print("Operação falhou! O valor informado para saque é inválido.")
 
@@ -45,9 +56,9 @@ while True:
         print(extrato)
         print(f"Saldo atual: R$ {saldo:.2f}")
         if numero_saques > 1:
-            print(f"Você pode realizar hoje {numero_saques} transações de saque em sua conta.")
+            print(f"Você pode realizar {numero_saques} transações de saque em sua conta.")
         elif numero_saques > 0:
-            print(f"Você pode realizar hoje {numero_saques} transação de saque em sua conta.")
+            print(f"Você pode realizar {numero_saques} transação de saque em sua conta.")
         else:
             print(f"Você não possui mais transação de saque permitida em sua conta nesta data.")
         print(f"Seu limite de saque por transação é R$ {limite:.2f}.")
