868d997d083df2a5e2aeb032f515ac26bda3b1fd
samuelsp <samp_81@yahoo.com.br>
2024-06-17 09:20:56 -0300

    rota de atleta

--- python-ai-backend-developer/workout/workout_api/atleta/controller.py
+++ python-ai-backend-developer/workout/workout_api/atleta/controller.py
@@ -1,17 +1,66 @@
 from fastapi import APIRouter, Body, HTTPException, status
-from workout_api.contrib.dependencies import DataBaseDependency
+from pydantic import UUID4
+from uuid import uuid4
+from datetime import datetime
+from sqlalchemy.future import select
+
+from workout_api.atleta.models import AtletaModel
 from workout_api.atleta.schemas import AtletaIn, AtletaOut
+from workout_api.categorias.models import CategoriaModel
+from workout_api.centro_treinamento.models import CentroTreinamentoModel
+
+from workout_api.contrib.dependencies import DataBaseDependency
+from sqlalchemy.future import select
 
 router = APIRouter()
 
-@router.post(path='/'
-             , summary='Criar novo atleta'
-             , status_code=status.HTTP_201_CREATED
-             #, response_model=AtletaOut
-             )
+@router.post(
+    '/',
+    summary='Criar um novo atleta',
+    status_code=status.HTTP_201_CREATED,
+    response_model=AtletaOut
+)
 async def post(
-           #db_session: DataBaseDependency,
-           #atleta_in: AtletaIn = Body(...)
-    ):
-    pass
+        db_session: DataBaseDependency,
+        atleta_in: AtletaIn = Body(...)
+):
+    categoria_nome = atleta_in.categoria.nome
+    centro_treinamento_nome = atleta_in.centro_treinamento.nome
+
+    categoria = (await db_session.execute(
+        select(CategoriaModel).filter_by(nome=categoria_nome))
+                 ).scalars().first()
+
+    if not categoria:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f'A categoria {categoria_nome} não foi encontrada.'
+        )
+
+    centro_treinamento = (await db_session.execute(
+        select(CentroTreinamentoModel).filter_by(nome=centro_treinamento_nome))
+                          ).scalars().first()
+
+    if not centro_treinamento:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f'O centro de treinamento {centro_treinamento_nome} não foi encontrado.'
+        )
+    try:
+        atleta_out = AtletaOut(id=uuid4(), created_at=datetime.utcnow(), **atleta_in.model_dump())
+        atleta_model = AtletaModel(**atleta_out.model_dump(exclude={'categoria', 'centro_treinamento'}))
+
+        atleta_model.categoria_id = categoria.pk_id
+        atleta_model.centro_treinamento_id = centro_treinamento.pk_id
+
+        db_session.add(atleta_model)
+        await db_session.commit()
+    except Exception:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail='Ocorreu um erro ao inserir os dados no banco'
+        )
+
+    return atleta_out
+
 
--- python-ai-backend-developer/workout/workout_api/atleta/models.py
+++ python-ai-backend-developer/workout/workout_api/atleta/models.py
@@ -3,8 +3,9 @@ from sqlalchemy import DateTime, ForeignKey, Integer, String, Float
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from workout_api.contrib.models import BaseModel
 
+
 class AtletaModel(BaseModel):
-    __tablename__ = "atletas"
+    __tablename__ = 'atletas'
 
     pk_id: Mapped[int] = mapped_column(Integer, primary_key=True)
     nome: Mapped[str] = mapped_column(String(50), nullable=False)
